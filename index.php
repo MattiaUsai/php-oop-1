4621f7ca8dcb8adf11063b9761bce5d9d096be4d
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-06-25 15:45:07 +0200

    ADDED MOVIE INSTANCE

--- index.php
+++ index.php
@@ -1,19 +1,36 @@
 <?php
 class Movie{
 
-    public $type;
-    public $relase_date;
+    public string $name;
+    public string $type;
+    public int $relase_date;
+    public int $valutation;
+    public int $duration;
+    
 
 
-    function __construct($type, $relase_date)
+    public function __construct($name,$type, $relase_date,$valutation,$duration)
     {
+        
+        $this->name = $name;
         $this->type = $type;
         $this->relase_date = $relase_date;
+        $this->valutation = $valutation;
+        $this->duration = $duration;
     }
+ 
+   
 
 }
 
+$list_movie=[
+    $spiderman = New Movie('Spiderman','fantasy',2010,6,2.25),
+    $superman = New Movie('Superman','fantasy',2008,6,1.50),
+    $batman = New Movie('Batman','fantasy',2012,6,2.30),
+    $flash = New Movie('Flash','fantasy',2017,6,1.25),
+    $thor = New Movie('Thor','fantasy',2011,6,0.40),
+    $supergirl = New Movie('Supergirl','fantasy',2020,6,2.00),
+];
 
-$spiderman = New Movie('action',2020);
 
-var_dump($spiderman);
+var_dump($list_movie);
